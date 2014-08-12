21b27482cca2e2a300722795858e6daf280e3164
Phani Raj <phanirajuyn@gmail.com>
2014-08-12 13:30:44 -0700

    disable edit/delete buttons if there is no selection

--- org.cloudfoundry.ide.eclipse.server.ui/src/org/cloudfoundry/ide/eclipse/server/ui/internal/EnvironmentVariablesPart.java
+++ org.cloudfoundry.ide.eclipse.server.ui/src/org/cloudfoundry/ide/eclipse/server/ui/internal/EnvironmentVariablesPart.java
@@ -90,8 +90,7 @@ public class EnvironmentVariablesPart extends UIPart {
 		Listener actionEnabler =  new Listener() {
 			@Override
 			 public void handleEvent(Event event) {
-			     removeEnvVarButton.setEnabled(isDeleteEnabled());
-			     editEnvVarButton.setEnabled(isEditEnabled());
+				setEnabledDisabled();
 			  }
 			 }; 
 			
@@ -138,6 +137,11 @@ public class EnvironmentVariablesPart extends UIPart {
 
 		AddEditButtons(parent);
 		return tableArea;
+	}
+
+	private void setEnabledDisabled() {
+		removeEnvVarButton.setEnabled(isDeleteEnabled());
+	    editEnvVarButton.setEnabled(isEditEnabled());
 	}
 	
 	private void AddEditButtons(Composite parent){
@@ -223,6 +227,7 @@ public class EnvironmentVariablesPart extends UIPart {
 		
 		if (variableChanged) {
 			notifyStatusChange(Status.OK_STATUS);
+			setEnabledDisabled();
 		}
 	}
 
@@ -237,6 +242,7 @@ public class EnvironmentVariablesPart extends UIPart {
 		
 		if (variableChanged) {
 			notifyStatusChange(Status.OK_STATUS);
+			setEnabledDisabled();
 		}
 	}
 
